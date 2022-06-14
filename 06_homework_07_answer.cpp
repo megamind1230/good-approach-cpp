1b2d9c3d63d336f5cbae5c4620ad59bb49c65f58
megamind1230 <90935539+megamind1230@users.noreply.github.com>
2022-06-14 18:59:36 +0200

    Create 06_homework_07_answer.cpp

---
+++ 06_homework_07_answer.cpp
@@ -0,0 +1,16 @@
+//even print 100 ,odd print 7
+#include <iostream>
+using namespace std;
+#define endl <<'\n';
+
+int main()
+{
+	int n;
+	cin>>n;
+	//notice they're int
+	int is_even = n % 2 == 0;
+	int is_odd = 1 - is_even;
+	// formula of 2 parts: only one of them will be activated
+	int result = is_even * 100 + is_odd * 7;
+	cout << result endl
+}
