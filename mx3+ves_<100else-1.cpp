f491c77b2c66aa89d9f08d589fa1e29cacbc5d84
megamind1230 <90935539+megamind1230@users.noreply.github.com>
2022-06-15 09:06:12 +0200

    Update mx3+ves_<100else-1.cpp

--- mx3+ves_<100else-1.cpp
+++ mx3+ves_<100else-1.cpp
@@ -1,24 +1,17 @@
 //07_homework_03_answer.cpp
 //max of 3 positive nums but max is less than 100 or print -1
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 int main() {
 	int a, b, c;
 	cin >> a >> b >> c;
-
 	int res = -1;
-
 	if (a < 100 && a > res)
 		res = a;
-
 	if (b < 100 && b > res)
 		res = b;
-
 	if (c < 100 && c > res)
 		res = c;
-
-	cout<<res<<"\n";
-
+	cout<<res<<'\n';
 	return 0;
 }
