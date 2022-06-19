fb6bedb0e96194330e204a3a8292d7178c937b0a
megamind1230 <90935539+megamind1230@users.noreply.github.com>
2022-06-19 12:26:59 +0200

    Update printNumsOfInt.cpp

--- printNumsOfInt.cpp
+++ printNumsOfInt.cpp
@@ -1,21 +1,21 @@
 //08_8_D.cpp
-///print the num of digits for an int
+//print the num of digits for an int
 #include<iostream>
 using namespace std;
 int main() {
 	int num;
-	cin >> num;
-	///tricky point.. notice that num with be zero later
-	///so we took a copy of it to print later
+	cin>>num;
+	//tricky point.. notice that num will be zero later
+	//so we took a copy of it to print later
 	int tem_num = num;
 	int digits = 0;
-	///special case 1
+	//special case 1
 	if (num == 0)
 		digits = 1;
-	///special case 2
+	//special case 2
 	else if (num == -2147483648)
 		digits = 10;
-	///core logic
+	//core logic
 	else {
 		if (num < 0)
 			num = -num;//make it +ve to work properly later on it
